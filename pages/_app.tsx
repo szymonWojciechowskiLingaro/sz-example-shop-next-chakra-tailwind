eb2af055da23d248a21f735b5ae06384d1660ef0
Szymon Wojciechowski <szymon.wojciechowski@lingarogroup.com>
2022-01-15 17:25:38 +0100

    settings for tsconfig and eslint. added layout all

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
-// import '../styles/globals.css';
 import '@styles//globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { chakraTheme } from '@pages/_app/config';
+import {useRef, FC} from "react";
+import {QueryClient} from "react-query";
+
+const Noop: FC = ({ children }) => <>{children}</>;
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const queryClientRef = useRef<any>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
   return (
     <ChakraProvider theme={chakraTheme}>
       <Component {...pageProps} />
