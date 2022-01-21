c3aeadba9984d3d8d1b5384c28c8cb969945d2fa
Szymon Wojciechowski <szymon.wojciechowski@lingarogroup.com>
2022-01-21 15:55:49 +0100

    feature cards blocks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ import HeroBlock from '@containers/HeroBlock'
 import TestimonialCarousel from '@containers/Testimonials'
 import { Container } from '@chakra-ui/react'
 import NewArrivalsProductFeed from "@components/product/new-arrivals-product-feed";
+import FeaturesBlock from "@containers/FeaturesBlock";
 
 const Home: NextPage = () => {
   return (
     <Container maxW='1920px' p={{ base: "16px", md: "32px", lg: "40px" }}>
       <HeroBlock />
       <NewArrivalsProductFeed />
+      <FeaturesBlock />
       <TestimonialCarousel sectionHeading="Testimonials" />
     </Container>
   )
